30637d3ac76900fd76bf1e8f2b02c1922c90a28d
ameliiapr <ameliatri2003@gmail.com>
2023-11-01 13:47:30 +0700

    klinik_baru

--- klinik/__manifest__.py
+++ klinik/__manifest__.py
@@ -9,17 +9,10 @@
     'depends': ['base'],
     'data' : [
       'views/klinik.xml',
+      'reports/report.xml',
+      'reports/surat_klinik.xml',
       'security/ir.model.access.csv'
     ]
-    # 'data': [
-    #     'views/plane.xml',
-    #     'views/template.xml',
-    #     'views/dashboard.xml',
-    #     'reports/report.xml',
-    #     'reports/boarding_pass.xml',
-    #     'data/data.xml',
-    #     'security/ir.model.access.csv'
-    # ],
     # 'qweb' : [
     #     'static/xml/*.xml',
     # ]
--- klinik/models/klinik.py
+++ klinik/models/klinik.py
@@ -5,11 +5,50 @@ class SuratIjin(models.Model):
     _description = "Surat Ijin Berobat"
 
     name = fields.Char(string='Nama Pasien', required=True)
-    nip = fields.Integer(string='NIP')
+    nip = fields.Char(string='NIP')
     tanggal = fields.Date(string='Tanggal')
     posisi = fields.Text(string='Job Position')
     kode_work_center = fields.Char(string='Kode Work Center')
-    no_kartu = fields.Integer(string='Nomor Kartu')
+    no_kartu = fields.Char(string='Nomor Kartu')
     keluhan = fields.Text(string='Keluhan Penyakit')
+    rujukan = fields.Text(string='Rumah Sakit Rujukan')
+    state = fields.Selection([ 
+        ('inprogress', 'Inprogress'),
+        ('confirm', 'Confirmed'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')
+    ], string='Status', default='inprogress', tracking=True)
+    tahap_waktu_berobat = fields.Selection([
+        ('kamar_kartu', 'Kamar Kartu'),
+        ('poli_umum', 'Poli Umum'),
+        ('poli_gigi', 'Poli Gigi'),
+        ('poli_paru', 'Poli Paru'),
+        ('poli_kb', 'Poli KB/BKIA'), 
+        ('kamar_dokter', 'Kamar Dokter'),
+        ('laboratorium', 'Laborotarium'),
+        ('ugada', 'UGADA/CHIRURGIE'),
+        ('kamar_suntik', 'Kamar Suntik'),
+        ('kamar_rontgen', 'Kamar Rontgen'),
+        ('kamar_keuring', 'Kamar Keuring'),
+        ('mrs./opname', 'MRS./OPNAME'),
+        ('apotik', 'Apotik')
+        ], string="Tahap Waktu Berobat") 
+    checkup = fields.Date(string='Tanggal Checkup')
+    jam = fields.Datetime(string="Jam")
+    diagnosa = fields.Text(string="Diagnosa")
+
+
+    # surat_ijin_ids = fields.One2many('surat.ijin.line', 'tahap_waktu_berobat', string="Problems")
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_done(self):
+        self.state = 'done'
     
-   
+    def action_inprogress(self):
+        self.state = 'inprogress'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+
